4b11f2bd77ad6c27248a986c146bd4bef42a7544
KalesseBen <kalesseben@outlook.com>
2025-04-14 14:57:31 +0200

    added MF_037.1

--- Frontend/src/components/mf/ManagementDrinking.vue
+++ Frontend/src/components/mf/ManagementDrinking.vue
@@ -9,6 +9,10 @@
                 question="Werden alle Tränkeeinrichtungen täglich auf Funktionalität und Durchflussgeschwindigkeit (ca. 7-15 l/min) kontrolliert?"
                 question-key="MF_037"
                 :store="managementStore" />
+            <RadioYesNo
+                question="Wird bei allen Tränkeeinrichtungen regelmäßig die Durchflussgeschwindigkeit (ca. 10-20 l/min) kontrolliert?"
+                question-key="MF_037.1"
+                :store="managementStore" />
         </template>
     </QuestionaireCard>
 </template>
